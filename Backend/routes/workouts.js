b08b3205993e8f1c0c627842415887543e3d550f
mostafa <mostafa.bebars37@gmail.com>
2025-11-01 00:14:56 +0200

    added file for routes and added some comments

---
+++ Backend/routes/workouts.js
@@ -0,0 +1,31 @@
+const express = require('express')
+
+const router = express.Router()
+
+// get all workouts
+router.get('/', (req, res) => {
+    res.json({mssg: 'Get all workouts'})
+})
+
+// get a single workout
+router.get('/:id', (req, res) => {
+    res.json({mssg: 'Get a single workout'})
+})
+
+// create a new workout
+router.post('/', (req, res) => {
+    res.json({mssg: 'Create a new workout'})
+})
+
+// delete a workout
+router.delete('/:id', (req, res) => {
+    res.json({mssg: 'Delete a workout'})
+})
+
+// update a workout
+router.patch('/:id', (req, res) => {
+    res.json({mssg: 'Update a workout'})
+})
+
+
+module.exports = router
--- Backend/server.js
+++ Backend/server.js
@@ -1,11 +1,23 @@
 require('dotenv').config()
 const express = require('express')
+const workourtRoutes = require('./routes/workouts')
+
+
+
 const app = express()
 
+// listne for requests
 app.listen(process.env.PORT, () => {
   console.log('Server is running on port ', process.env.PORT)
 })
 
-app.get('/', (req, res) => {
-  res.json({mssg:'Hello World!'})
+// middleware
+app.use(express.json())
+app.use((req, res, next) => {
+    console.log(req.path, req.method)
+    next()
 })
+
+
+// routes 
+app.use('/api/workouts', workourtRoutes) 
