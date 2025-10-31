1c402f0b8a9f134a011aab336e21536cc3af453d
mostafa <mostafa.bebars37@gmail.com>
2025-11-01 00:55:23 +0200

    added mongoDB

--- Backend/server.js
+++ Backend/server.js
@@ -1,15 +1,11 @@
 require('dotenv').config()
 const express = require('express')
+const mongoose = require('mongoose')
 const workourtRoutes = require('./routes/workouts')
 
 
 
 const app = express()
-
-// listne for requests
-app.listen(process.env.PORT, () => {
-  console.log('Server is running on port ', process.env.PORT)
-})
 
 // middleware
 app.use(express.json())
@@ -21,3 +17,15 @@ app.use((req, res, next) => {
 
 // routes 
 app.use('/api/workouts', workourtRoutes) 
+
+// connect to db
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => {
+        // listen for requests
+        app.listen(process.env.PORT, () => {
+            console.log('Connected to DB and listening on port', process.env.PORT)
+        })
+    })
+    .catch((error) => {
+        console.log(error)
+    })
