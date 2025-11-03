4ccc4506217a5c75fd82e57b937eb0c0cbdc3cb8
mostafa <mostafa.bebars37@gmail.com>
2025-11-03 05:49:49 +0200

    finished the frontend and user interface part

--- Backend/server.js
+++ Backend/server.js
@@ -1,33 +1,33 @@
 const cors = require("cors")
 require('dotenv').config()
+
 const express = require('express')
 const mongoose = require('mongoose')
-const workourtRoutes = require('./routes/workouts')
+const workoutRoutes = require('./routes/workouts')
 
-
-
+// express app
 const app = express()
 
 // middleware
-app.use(cors())
 app.use(express.json())
+app.use(cors())
 app.use((req, res, next) => {
-    console.log(req.path, req.method)
-    next()
+  console.log(req.path, req.method)
+  next()
 })
 
-
-// routes 
-app.use('/api/workouts', workourtRoutes) 
+// routes
+app.use('/api/workouts', workoutRoutes)
 
 // connect to db
 mongoose.connect(process.env.MONGO_URI)
-    .then(() => {
-        // listen for requests
-        app.listen(process.env.PORT, () => {
-            console.log('Connected to DB and listening on port', process.env.PORT)
-        })
+  .then(() => {
+    console.log('connected to database')
+    // listen to port
+    app.listen(process.env.PORT, () => {
+      console.log('listening for requests on port', process.env.PORT)
     })
-    .catch((error) => {
-        console.log(error)
-    })
+  })
+  .catch((err) => {
+    console.log(err)
+  }) 
