275ab653b101124ef0216c29431d45f279053382
mostafa <mostafa.bebars37@gmail.com>
2025-11-25 15:56:51 +0200

    email and password validation

--- Backend/controllers/userController.js
+++ Backend/controllers/userController.js
@@ -1,17 +1,29 @@
 const User = require('../models/userModel')
 const mongoose = require('mongoose')
 
+
+
+// login user
 const loginUser = async (req, res) => {
     res.json({msg: 'login user'})
 }
 
+
+// signup user
 const signupUser = async (req, res) => {
-    res.json({msg: 'signup user'})
+    const {email, password} = req.body
+
+    try {
+        const user = await User.signup(email, password)
+        res.status(200).json({email, user})
+    } catch (error) {
+        res.status(400).json({error: error.message})
+    }
+
+
+
+    
 }
-
-
-
-
 
 
 
