f23914b48edf0d3abbdc0fab04cb9ab838e8233c
mostafa <mostafa.bebars37@gmail.com>
2025-11-25 04:12:57 +0200

    some setup for the user

---
+++ Backend/controllers/userController.js
@@ -0,0 +1,21 @@
+const User = require('../models/userModel')
+const mongoose = require('mongoose')
+
+const loginUser = async (req, res) => {
+    res.json({msg: 'login user'})
+}
+
+const signupUser = async (req, res) => {
+    res.json({msg: 'signup user'})
+}
+
+
+
+
+
+
+
+module.exports = {
+    loginUser,
+    signupUser,
+}
