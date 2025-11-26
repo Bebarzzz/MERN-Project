632cab4ad4fdd68c294b76328830136faadab902
mostafa <mostafa.bebars37@gmail.com>
2025-11-26 23:43:44 +0200

    user id to each workout

--- frontend/src/components/WorkoutForm.js
+++ frontend/src/components/WorkoutForm.js
@@ -34,7 +34,7 @@ const WorkoutForm = () => {
 
     if (!response.ok) {
       setError(json.error)
-      setEmptyFields(json.emptyFields)
+      setEmptyFields(json.emptyFields || [])
     }
     if (response.ok) {
       setTitle('')
